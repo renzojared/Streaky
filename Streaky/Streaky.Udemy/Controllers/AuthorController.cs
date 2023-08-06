cfa16123fa3f34a8f48fbb0de6c9e0d4ac1def14
Renzo Jared <renzojared_lm@hotmail.com>
2023-08-05 21:17:58 -0500

    first api simple get

---
+++ Streaky/Streaky.Udemy/Controllers/AuthorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streaky.Udemy.Entities;
+
+namespace Streaky.Udemy.Controllers;
+
+[ApiController]
+[Route("api/authors")]
+public class AuthorController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<Author>> Get()
+    {
+        return new List<Author>()
+        {
+            new Author() {Id = 1, Name = "Renzo"},
+            new Author() {Id = 2, Name = "Karina"}
+        };
+    }
+}
+
---
+++ Streaky/Streaky.Udemy/Entities/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace Streaky.Udemy.Entities;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
