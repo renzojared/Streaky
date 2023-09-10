423028276134c3c827cb856e2e9b32b00b64c24b
Renzo Jared <renzojared_lm@hotmail.com>
2023-09-09 19:36:35 -0500

    New final proyect

---
+++ Streaky/Streaky.Movies/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace Streaky.Movies;
+
+public class Startup
+{
+    public IConfiguration configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "WebApiMovies", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(u => u.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiMovies v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(e =>
+        {
+            e.MapControllers();
+        });
+    }
+}
+
